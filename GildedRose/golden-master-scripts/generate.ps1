11ae341cc20d0378eb7e8c82a2e1aba4ac0f1573
U-Ian-HP\Ian <Ian@Ian-HP.(none)>
2013-04-22 18:24:14 +0100

    Improvements to the power shell version of the generate command.

--- GildedRose/golden-master-scripts/generate.ps1
+++ GildedRose/golden-master-scripts/generate.ps1
@@ -1,17 +1,18 @@
 # Based on scripts from https://github.com/SamirTalwar/trivia-golden-master.git
 
-
-if ($args.Count -ne 4) 
+if ($args.Count -ne 2)
 { 
 	echo "Usage: generate \""your command here\"" \""output directory\"""
+	exit
 } 
-
-
 
 $command=$args[0]
 $dir=$args[1]
 
 echo $dir
 
-mkdir -p $dir\golden-master
+if (!(Test-Path $dir))
+{
+	mkdir -p $dir
+}
 iex $command > $dir/golden-master
