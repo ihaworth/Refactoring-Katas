5c0a298ec207329ffceaf3e9015306c07d1c4e7d
U-Ian-HP\Ian <Ian@Ian-HP.(none)>
2013-04-23 12:52:05 +0100

    Add a windows power shell version of the verify command

--- GildedRose/golden-master-scripts/generate.ps1
+++ GildedRose/golden-master-scripts/generate.ps1
@@ -2,14 +2,12 @@
 
 if ($args.Count -ne 2)
 { 
-	echo "Usage: generate \""your command here\"" \""output directory\"""
+	echo "Usage: generate.ps1 \""your command here\"" \""output directory\"""
 	exit
 } 
 
 $command=$args[0]
 $dir=$args[1]
-
-echo $dir
 
 if (!(Test-Path $dir))
 {
